2a6f68114b8e0e47fabe32f1f2d962ad2f599386
bjornclauw <32811735+bjornclauw@users.noreply.github.com>
2025-10-26 16:38:55 +0100

    Update styles.d.ts

--- src/styles.d.ts
+++ src/styles.d.ts
@@ -1,5 +1,1 @@
 declare module '*.css';
-
-// This file tells TypeScript that importing CSS files (side-effect imports)
-// is allowed. It's small and safe for Obsidian plugin projects that bundle
-// CSS with esbuild.
